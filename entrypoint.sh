16fbf0bcb6955b49c5181c189c3c6698cde6882e
Alexei Fedotov <alexei.fedotov@gmail.com>
2022-08-11 15:23:54 +0200

    added reregistration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/env bash
 set -eu
 
-cd /build-runner
+restart_agent() {
+    test -f url || return
+    test -f pid && kill $(cat pid)
+    test -f token.running && ./config.sh remove --token $(cat token.running)
+    mv token token.running
+    ./config.sh --url $(cat url) --token $(cat token.running)
 
-test -f registered || {
-    ./config.sh --url https://github.com/intel-ai/omniscidb --token $TOKEN
-    touch registered
+    (
+        while true
+        do
+            sleep 10
+            ./run.sh
+        done
+    ) &
+    echo $! >pid
 }
 
-while true
-do
-    ./run.sh
-    sleep 10
-done
+run_watchdog() {
+    cd /build-runner
+    while true
+    do
+        sleep 1
+        test -f token || continue
+        restart_agent
+    done
+}
+
+run_watchdog
+
+
 
