43440f38e999ca4d4d2c78c714494cbc58baf720
Alexei Fedotov <alexei.fedotov@gmail.com>
2023-02-20 19:14:08 -0800

    Fix getting agent version

--- rebuild.sh
+++ rebuild.sh
@@ -13,7 +13,7 @@ touch ~/.docker/config.json
 cp ~/.docker/config.json .
 
 gid=$(stat -c "%g" /var/run/docker.sock)
-gversion=$(curl -X HEAD -i https://github.com/actions/runner/releases/latest | awk '/^location: / { gsub(/.*v/, ""); gsub(/[^0-9]*$/, ""); print }')
+gversion=$(curl --head -i https://github.com/actions/runner/releases/latest | awk '/^[lL]ocation: / { gsub(/.*v/, ""); gsub(/[^0-9]*$/, ""); print }')
 
 cd $(dirname "$0")
 
