a98dd2abdbf642377e0c57040a1a27e2778d0ef5
Musin, Vadim <vadim.musin@intel.com>
2023-03-27 20:38:28 +0200

    Fix pid handling

--- update-token.sh
+++ update-token.sh
@@ -11,7 +11,9 @@ url=https://github.com/$(echo "$CI_DOCKER" | sed -e 's#/[^/]*$##')
 
 register_runner() {
     token=$(cat "$name".token)
-    docker exec -t $name sh -c "cd /build-runner && ./config.sh --name $name --labels $name --url $url --token $token --unattended && kill \$(cat pid) || true"
+    internal_pid=$(docker exec -t $name cat pid || true)
+    docker exec -t $name sh -c "cd /build-runner && ./config.sh --name $name --labels $name --url $url --token $token --unattended || true"
+    test ! -z "$internal_pid" && docker exec -t $name kill $internal_pid
 }
 
 sh -$- unregister.sh "$name"
